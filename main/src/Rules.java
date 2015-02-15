3047595db5f1245023ad66b53e2e07bf06bec515
DiagonalFra <DiagonalFra@users.noreply.github.com>
2015-02-15 14:10:39 +0100

    try at section

--- main/src/Rules.java
+++ main/src/Rules.java
@@ -1,75 +1,110 @@
+import java.util.ArrayList;
 
 public class Rules {
 
-    public boolean isValidBoard(final byte[][] boardState) {
+	public boolean isValidBoard(final byte[][] boardState) {
 
-        boolean isValid = false;
+		boolean isValid = true;
 
-        // todo fra :
-        /* using the methods in this class you need to work out whether the parsed boardState byte[][] is a
-         * valid sudoku game and parse true if it is.
-         * Also work out which methods you think should only be called in this class only (and make them private), and
-         * which methods need to be called outside of this class (in GameFrame) and make them public */
+		byte[] tempRow = new byte[9];
+		byte[] tempCol = new byte[9];
 
-        return isValid;
-    }
+		for (int a = 0; a < boardState.length; a++) {
+			for (int b = 0; b < boardState.length; b++) {
+				tempRow[b] = boardState[a][b];
+				tempCol[b] = boardState[b][a];
+			}
+			if (isValidArray(tempRow) == false
+					|| isValidArray(tempCol) == false) {
+				isValid = false;
+			}
+		}
 
-    // always 9 places, range valid, number once
-    public boolean isValidArray(final byte[] array) {
-        if (array.length == 9 && isRangeValid(array) && isNumberOnce(array)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+		/*
+		 * using the methods in this class you need to work out whether the
+		 * parsed boardState byte[][] is a valid sudoku game and parse true or
+		 * false
+		 */
 
-    // need to convert section 2D array => 1D array, so can call same method
-    public byte[] convertTo1D(final byte[][] section) {
-        byte[] oneDSection = new byte[section.length * section.length];
-        for (int i = 0; i < section.length; i++) {
-            for (byte j = 0; j < section.length; j++) {
-                oneDSection[(i * section.length) + j] = section[i][j];
-            }
-        }
+		boolean sectionValid = isValidArray(getSection(boardState));
 
-        return oneDSection;
-    }
+		return isValid;
+	}
 
-    // numbers 1-9
-    public boolean isRangeValid(final byte[] array) {
-        boolean isRangeValid = false;
-        for (int i = 0; i < array.length; i++) {
-            byte currentNumber = array[i];
+	// always 9 places, range valid, number once
+	private boolean isValidArray(final byte[] array) {
+		if (array.length == 9 && isRangeValid(array) && isNumberOnce(array)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-            // check if element is VALID 1-9
-            if (array[i] >= 0 && array[i] < 10) {
-                isRangeValid = true;
-            } else {
-                isRangeValid = false;
-                return false;
-            }
-        }
+	// get first Section
+	public byte[] getSection(final byte[][] boardState) {
 
-        return isRangeValid;
-    }
+		byte[] section = new byte[9];
+		int count = 0;
 
-    // Numbers only once
-    public boolean isNumberOnce(final byte[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
 
-            byte elementToCheck = array[i];
+				section[count] = boardState[i][j];
+				count++;
+			}
 
-            if (elementToCheck != 0) {
+		}
 
-                for (int j = i + 1; j < array.length; j++) {
+		return section;
+	}
 
-                    if (elementToCheck == array[j]) {
-                        return false;
-                    }
-                }
-            }
-        }
+	// need to convert section 2D array => 1D array, so can call same method
+	private byte[] convertTo1D(final byte[][] section) {
+		byte[] oneDSection = new byte[section.length * section.length];
+		for (int i = 0; i < section.length; i++) {
+			for (byte j = 0; j < section.length; j++) {
+				oneDSection[(i * section.length) + j] = section[i][j];
+			}
+		}
 
-        return true;
-    }
+		return oneDSection;
+	}
+
+	// numbers 1-9
+	private boolean isRangeValid(final byte[] array) {
+		boolean isRangeValid = false;
+		for (int i = 0; i < array.length; i++) {
+			byte currentNumber = array[i];
+
+			// check if element is VALID 1-9
+			if (array[i] >= 0 && array[i] < 10) {
+				isRangeValid = true;
+			} else {
+				isRangeValid = false;
+				return false;
+			}
+		}
+
+		return isRangeValid;
+	}
+
+	// Numbers only once
+	private boolean isNumberOnce(final byte[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+
+			byte elementToCheck = array[i];
+
+			if (elementToCheck != 0) {
+
+				for (int j = i + 1; j < array.length; j++) {
+
+					if (elementToCheck == array[j]) {
+						return false;
+					}
+				}
+			}
+		}
+
+		return true;
+	}
 }
